823f0f0e9fa7227924fe20d0aeb6c1cd04086f4b
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-17 17:33:12 +0600

    Update Index.php
    Single line comments start with // or #.
    
    Any text after the // or # - and to the end of line, will be ignored.

--- Index.php
+++ Index.php
@@ -5,6 +5,9 @@
 <h1>My first PHP page</h1>
 
 <?php
+// Output welcome message
+echo 'Welcome Home!';
+echo 'Welcome Home!'; // Output welcome message
 $color = "red";
 echo "My car is $color<br>";
 echo "My house is $COLOR";
