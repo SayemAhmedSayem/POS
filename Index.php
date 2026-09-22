b5de630321034ac4bb34ef78360371931f6a53f7
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-23 00:35:15 +0600

    Update Index.php
    The var_dump() function returns the data type and the value

--- Index.php
+++ Index.php
@@ -25,6 +25,14 @@ echo "I love $txt!";*/
 $x = 5;
 $y = 4;
 echo $x + $y;
+
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+
 ?>
 
 </body>
